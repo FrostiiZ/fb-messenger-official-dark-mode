a331ad9ab6c44e473fa830122cdbed720af551f9
FrostiiZ <frostiizedits@gmail.com>
2020-12-14 14:48:38 +0100

    Added the Tampermonkey script file
    Contains the file that must be given to Tampermonkey in order to add Facebook's messenger.com Dark Mode class to the html body.

---
+++ Messenger.com Force Official Dark Mode (M-FODM).user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Messenger.com Force Official Dark Mode (M-FODM)
+// @namespace    https://github.com/FrostiiZ/
+// @version      0.1
+// @description  Append the "__fb-dark-mode" class to the HTML tag and force Facebook's official Dark Mode
+// @author       FrostiiZ
+// @match        https://www.messenger.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.querySelector('html').classList.add('__fb-dark-mode');
+})();
