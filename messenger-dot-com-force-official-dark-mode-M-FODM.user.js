f3a7cef449550103f231e4cd7c9ccbca7b97e096
FrostiiZ <frostiizedits@gmail.com>
2020-12-14 15:30:16 +0100

    Update messenger-dot-com-force-official-dark-mode-M-FODM.user.js

--- messenger-dot-com-force-official-dark-mode-M-FODM.user.js
+++ messenger-dot-com-force-official-dark-mode-M-FODM.user.js
@@ -3,6 +3,11 @@
 // @namespace    https://github.com/FrostiiZ/
 // @version      0.2
 // @description  Append the "__fb-dark-mode" class to the HTML tag and force Facebook's official Dark Mode
+// @encoding utf-8
+// @homepage https://github.com/FrostiiZ/fb-messenger-official-dark-mode
+// @supportURL https://github.com/FrostiiZ/fb-messenger-official-dark-mode/issues
+// @updateURL https://github.com/FrostiiZ/fb-messenger-official-dark-mode/blob/main/messenger-dot-com-force-official-dark-mode-M-FODM.user.js
+// @downloadURL https://github.com/FrostiiZ/fb-messenger-official-dark-mode/blob/main/messenger-dot-com-force-official-dark-mode-M-FODM.user.js
 // @author       FrostiiZ
 // @match        https://www.messenger.com/*
 // @grant        none
